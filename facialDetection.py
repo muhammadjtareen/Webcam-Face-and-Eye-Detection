72b371a1bc232cb2a76c39a3c4c2512497b1b4e3
muhammadjtareen <115113407+muhammadjtareen@users.noreply.github.com>
2022-10-05 17:49:31 -0400

    Update facialDetection.py

--- facialDetection.py
+++ facialDetection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #accesses camera
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
